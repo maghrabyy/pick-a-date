8a94cf01ded07ce59a4646b778f22d8a829df66c
Maghrabyy <mahmoud.elmaghraby11@gmail.com>
2024-05-07 20:53:07 +0300

    update img

--- src/AskForDate.tsx
+++ src/AskForDate.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import tekaTakaImg from '../src/assets/tekataka.png';
+import ashrafleoImg from '../src/assets/ashrf.png';
 
 type AskForADateType = {
   yesClickedCallbk:()=>void
@@ -14,12 +14,12 @@ export const AskForADate = ({yesClickedCallbk}:AskForADateType)=>{
         e.target.style.top = `${randYNum}px`
         e.target.style.left = `${randXNum}px`;
       }
-    return <div className="ask-for-a-date px-2">
+    return <div className="ask-for-a-date px-2 flex flex-col">
     <h1 className='sm:text-4xl text-[1.4rem] text-center py-4'>Do you wanna go on a date with me?</h1>
     <div className={`flex gap-2 ${isHovered? 'static' : 'relative'}`}>
       <button onClick={yesClickedCallbk} className='yesBtn bg-green-500 hover:bg-green-400 text-white px-4 py-2 rounded-md font-semibold shadow-sm shadow-green-700'>Yes</button>
       <button onMouseEnter={changeNoBtnPosition} onClick={changeNoBtnPosition} className='noBtn bg-red-500 text-white px-4 py-2 rounded-md font-semibold shadow-sm shadow-red-700 absolute left-[calc(100%-55px)] duration-300 ease-out'>No</button>
     </div>
-    <img src={tekaTakaImg} alt="teka taka" />
+    <img src={ashrafleoImg} width={250} className="self-center" alt="ashraf leo" />
 </div>
 } 
